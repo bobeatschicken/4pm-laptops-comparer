1ca9f7c1cdbb489e93e2c1fea66c5ebf2f0bf8ca
Tony Sun <tonysun@ucsb.edu>
2020-02-10 17:58:49 -0800

    internal function for workspace to update product collection

--- backend/scrape_amazon/update_product_db_amazon.py
+++ backend/scrape_amazon/update_product_db_amazon.py
@@ -1,46 +1,24 @@
 from scrape_objects_MVP import get_attributes
 from pymongo import MongoClient
 
-def setup():
+CLIENT = MongoClient("mongodb+srv://XXXXXX:XXXXXXX@cluster0-swsn8.mongodb.net/test?retryWrites=true&w=majority")
+DB = CLIENT.compurator
+PRODUCTS_COLLECTION = DB["products"]
+
+
+def add_product_amazon(PRODUCTS_COLLECTION, url):
     '''
-    set up client for mongo DB and read URL text file
-
-    :return: products_collection, list of lines from text file of URLs
-    '''
-    client = MongoClient("mongodb+srv://XXXXXX:XXXXXXX@cluster0-swsn8.mongodb.net/test?retryWrites=true&w=majority")
-    db = client.compurator
-    products_collection = db["products"]
-
-    file_object = open('laptop_urls.txt')
-    lines = file_object.readlines()
-
-    return products_collection, lines
-
-def add_product_amazon(products_collection, lines):
-    '''
-    adds product documents to mongo DB product collection based on list of URLs
-    in the text file, URLs above the --- denote unadded products
-
-    :param lines: list of lines from input text file of URLs
-    :return: None
+    :param PRODUCTS_COLLECTION, url:
+    :return prod_document: dictionary containing attributes of product on amazon
     '''
 
-    for line in lines:
-        if line == '---':
-            break
+    prod_document = get_attributes(url)
 
-        url = str(line.strip())
-        prod_document = get_attributes(url)
+    # product already exists in database
+    # remove it in case price has been updated, then re-add. Can optimize by checking whether or not price has changed, but can fix later
+    if PRODUCTS_COLLECTION.find({'p_id': prod_document['p_id']}) is not None:
+        PRODUCTS_COLLECTION.delete_one({'p_id': prod_document['p_id']})
 
-        # product already exists in database
-        # remove it in case price has been updated, then re-add. Can optimize by checking whether or not price has changed, but can fix later
-        if products_collection.find({'p_id': prod_document['p_id']}) is not None:
-            products_collection.delete_one({'p_id': prod_document['p_id']})
+    PRODUCTS_COLLECTION.insert_one(prod_document)
 
-        products_collection.insert_one(prod_document)
-
-        # print(url)
-
-if __name__=='__main__':
-    products_collection, lines = setup()
-    add_product_amazon(products_collection, lines)
+    return prod_document
