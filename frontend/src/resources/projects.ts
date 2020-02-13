db4104d9a23053707a26d3bd44a3d8106436a18a
Bryan Terce <bryan.terce@gmail.com>
2020-02-13 01:03:03 -0800

    Create useWorkspace hook

--- frontend/src/resources/projects.ts
+++ frontend/src/resources/projects.ts
@@ -53,3 +53,29 @@ export function useCreateWorkspace() {
     run()
   }, [run])
 }
+
+export function useWorkspace(workspaceId: string) {
+  const { token, loaded } = useAuth()
+
+  const [{ data, error }, run] =
+    useAxios({
+      url: `${BASE_URL}/workspaces/${workspaceId}`,
+      headers: {
+        Authorization: token
+      }
+    }, { manual: true })
+
+  useEffect(() => {
+    if (loaded) {
+      if (token) {
+        run()
+      }
+    }
+  }, [loaded, run, token])
+
+  return {
+    data,
+    error: data?.error,
+    loading: !data && !error
+  }
+}
