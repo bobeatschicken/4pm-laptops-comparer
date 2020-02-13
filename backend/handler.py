91a0da4aea72b0ee639bf093da76f9fa6e134f1c
Jayleen Li <jayleenli17@gmail.com>
2020-02-12 20:20:46 -0800

    WIP JL - fixed get /workspaces endpoint

--- backend/handler.py
+++ backend/handler.py
@@ -8,7 +8,7 @@ GET /workspaces endpoint
 	Also if the user has never logged into the account before, it will create on in the users collection
 
  '''
-def return_workspaces(event, context):
+def get_workspaces(event, context):
 
 	#Connect to MongoDB
 	db_url = os.environ['DB_URL']
@@ -38,7 +38,77 @@ def return_workspaces(event, context):
 	cursor = workspace_collection.find({"owner": user_google_id})
 	#If there is no workspaces return no workspaces
 	if (cursor.count() == 0):
-		body = {"message":"This user does not have any workspaces set up."}
+		body = {"workspaces":[]}
+	else:
+		#Return workspaces of the user
+		build_body = {"workspaces": [] }
+
+		for workspace in cursor:
+			print("workspace", workspace)
+			build_workspace = {"workspace_id": 0, "products": []}
+
+			build_workspace["workspace_id"] = str(workspace["_id"])
+			#Go through the products array and add product info
+			products = workspace["products"]
+
+			for p_id in products:
+				#Get info from products collection
+				product_cursor = products_collection.find({"p_id": p_id})
+
+				if (product_cursor.count() == 0):
+					#for some reason the product does not exist in product collection
+					error = {"error": "This product does not exist."}
+					build_workspace["products"].append(error)
+				else:
+					#a product is returned, there should not be more than one product per product id
+					#Only for loop once but idk how to return just one
+					for product in product_cursor:
+						del product["_id"] #Deleted the _id that is auto generated from Mongo
+						build_workspace["products"].append(product)
+			build_body["workspaces"].append(build_workspace)
+		body = build_body
+
+		print(body)
+
+	response = {
+		"statusCode": 200,
+		"body": json.dumps(body)
+	}
+
+	return response
+
+
+''' 
+GET /workspaces/{workspaceId} endpoint
+	Given a correct Google JWT token for a user, this endpoint will return the workspaces of the specified ID
+	Should only work if the user owns that workspace
+
+ '''
+def get_workspace_by_id(event, context):
+
+	#Connect to MongoDB
+	db_url = os.environ['DB_URL']
+	print(f"Connecting to {db_url}")
+	client = MongoClient(db_url)
+	db = client.compurator
+	users_collection = db["users"]
+	workspace_collection = db["workspaces"]
+	products_collection = db["products"]
+
+	body = ""
+
+	authorizer_response = event
+
+	#Event is the response returned by the authorizer
+	print("requestContext: ", authorizer_response["requestContext"]["authorizer"])
+	user_google_id = authorizer_response["requestContext"]["authorizer"]["user_id"]
+	user_name = authorizer_response["requestContext"]["authorizer"]["user_name"]
+
+	#Then find the workspaces for this user
+	cursor = workspace_collection.find({"owner": user_google_id})
+	#If there is no workspaces return no workspaces
+	if (cursor.count() == 0):
+		body = {"message":"Workspace does not exist or this user does not own this workspace."}
 	else:
 		#Return workspaces of the user
 		build_body = {"workspaces": [] }
